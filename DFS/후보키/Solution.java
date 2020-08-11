52600141b29c301a1412bafa21c3006cf6109d5c
l100320503 <l10032503@gmail.com>
2020-08-11 20:11:38 +0900

    add(DFS/후보키/현정민)

---
+++ DFS/후보키/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+
+}
