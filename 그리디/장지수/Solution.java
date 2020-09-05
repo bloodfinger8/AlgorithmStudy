4820410f448996681ed1fe67b0be94e5dc005b4f
JisooJang <ppuagirls@naver.com>
2020-09-05 22:55:37 +0900

    add(그리디/ATM/장지수)

---
+++ 그리디/장지수/Solution.java
@@ -0,0 +1,27 @@
+package 그리디.장지수;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int[] arr = new int[num];
+        for(int i=0 ; i<num ; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println(getMinSum(arr, num));
+    }
+    public static int getMinSum(int[] arr, int num) {
+        int[] arr2 = new int[arr.length];
+        int sum = 0;
+        Arrays.sort(arr);
+        for(int i=0 ; i<arr.length ; i++) {
+            sum += arr[i];
+            arr2[i] = sum;
+        }
+        return Arrays.stream(arr2).sum();
+    }
+}
+
