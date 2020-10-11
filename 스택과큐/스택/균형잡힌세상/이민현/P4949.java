8da36e0079a64d19023a8bed6aba4d979d7c477d
Minhyun Lee <9m1i9n1@gmail.com>
2020-10-11 11:39:00 +0900

    add(힙/더맵게/이민현)

--- 스택과큐/스택/균형잡힌세상/이민현/P4949.java
+++ 스택과큐/스택/균형잡힌세상/이민현/P4949.java
@@ -7,40 +7,70 @@ public class P4949 {
     static final String YES = "yes";
     static final String NO = "no";
 
-    public void solution() {
-        String sentence;
-        Stack<Character> stack = new Stack<>();
+    // public void solution() {
+    //     String sentence;
+    //     Stack<Character> stack = new Stack<>();
 
-        Scanner input = new Scanner(System.in);
+    //     Scanner input = new Scanner(System.in);
 
-        while(true) {
-            sentence = input.nextLine();
-            if(sentence.equals(".")) break;
+    //     while(true) {
+    //         sentence = input.nextLine();
+    //         if(sentence.equals(".")) break;
 
-            boolean isBalanced = true;
-            char text;
+    //         boolean isBalanced = true;
+    //         char text;
 
-            for(int i = 0; i < sentence.length(); i++) {
-                text = sentence.charAt(i);
+    //         for(int i = 0; i < sentence.length(); i++) {
+    //             text = sentence.charAt(i);
 
-                if(text == '(' || text == '[') {
-                    stack.push(text);
-                } else if (text == ')') {
-                    if(stack.isEmpty() || stack.pop() != '(') {
-                        isBalanced = false;
-                        break;
-                    }
-                } else if (text == ']') {
-                    if(stack.isEmpty() || stack.pop() != '[') {
-                        isBalanced = false;
-                        break;
-                    }
-                }
-            }
+    //             if(text == '(' || text == '[') {
+    //                 stack.push(text);
+    //             } else if (text == ')') {
+    //                 if(stack.isEmpty() || stack.pop() != '(') {
+    //                     isBalanced = false;
+    //                     break;
+    //                 }
+    //             } else if (text == ']') {
+    //                 if(stack.isEmpty() || stack.pop() != '[') {
+    //                     isBalanced = false;
+    //                     break;
+    //                 }
+    //             }
+    //         }
 
-            if(!stack.isEmpty()) isBalanced = false;
-            System.out.println(isBalanced ? YES : NO);
-            stack.clear();
+    //         if(!stack.isEmpty()) isBalanced = false;
+    //         System.out.println(isBalanced ? YES : NO);
+    //         stack.clear();
+    //     }
+    // }
+
+    public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            String sentence = sc.nextLine();
+            System.out.println(isBalanced(sentence) ? "YES" : "NO");
         }
+
     }
+    public static boolean isBalanced(String sentence) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0 ; i<sentence.length() ; i++) {
+            char c = sentence.charAt(i);
+            if(c == '(' || c == '[') {
+                st.push(c);
+            } else if(c == ')') {
+                if(st.isEmpty()) return false;
+                char c2 = st.pop();
+                if(c2 != '(') return false;
+
+            } else if(c == ']') {
+                if(st.isEmpty()) return false;
+                char c2 = st.pop();
+                if(c2 != '[') return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
 }
